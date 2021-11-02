3ba24e4f5bb183ef726853b7e97210428a1656f8
scarantula <93314633+scarantula@users.noreply.github.com>
2021-11-02 09:03:28 +0000

    Add files via upload

---
+++ Flash Chat iOS13/MessageCell.swift
@@ -0,0 +1,32 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by user205198 on 10/20/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Flash Chat iOS13/MessageModel.swift
@@ -0,0 +1,12 @@
+//
+//  MessageModel.swift
+//  Flash Chat iOS13
+//
+//  Created by user205198 on 10/20/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct Message{
+    let sender: String
+    let body: String
+}
